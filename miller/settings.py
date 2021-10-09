3e9aea7be9e3adf679e8a0308f6f8a3cdc2f2ef1
Michael Thomas <mt@mthomas-it.de>
2021-10-09 15:33:47 +0200

    chore: settings.py new from old repo

--- miller/settings.py
+++ miller/settings.py
@@ -111,13 +111,23 @@ WSGI_APPLICATION = 'miller.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'TEST': {
+#             'NAME':  os.path.join(BASE_DIR, 'test.db.sqlite3'),
+#         }
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TEST': {
-            'NAME':  os.path.join(BASE_DIR, 'test.db.sqlite3'),
-        }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bibe',
+        'USER': 'miller',
+        'PASSWORD': 'miller',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
